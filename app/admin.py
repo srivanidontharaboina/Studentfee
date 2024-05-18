11fa4ac9b66770fd0dff2a80a40749c6e89e2a1f
srivanidontharaboina <srivanidontharaboina95@gmail.com>
2024-05-18 16:33:50 +0530

    first commit

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+# Register your models here.
+from app.models import Task
+class TaskAdmin(admin.ModelAdmin):
+    list_display=('task','is_completed','updated_at')
+    search_fields=('task',)
+admin.site.register(Task,TaskAdmin)
